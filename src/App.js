a3a6fa3b46361f8f09e2fc2ac6d41f500ef05ab9
Jefferson <jefry04@gmail.com>
2021-03-16 19:05:18 -0500

    onChange

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       terms: false,
       showing: false,
     };
   }
 
-  showInfo(event) {
+  showInfo = (event) => {
     this.setState({
       terms: event.target.checked,
       showing: !this.state.showing,
     });
-  }
+  };
 
   render() {
     return (
@@ -23,12 +23,22 @@ class App extends Component {
         <label>
           <input
             type="checkbox"
-            checked={this.state.terms}
-            onClick={this.showInfo.bind(this)}
+            defaultChecked={this.state.terms}
+            onChange={this.showInfo}
           />
           Mostrar información importante
         </label>
-        {this.state.showing ? <p>This is visible</p> : null}
+        {this.state.showing ? (
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        ) : null}
       </div>
     );
   }
